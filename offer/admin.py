90aaad52c604ff50f8f4b7d8670d4afb20b5a572
Pavel Tabakov <pavel.ivanov.tabakov@gmail.com>
2017-10-01 16:08:38 +0300

    Veltasoft LP

---
+++ offer/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Offer
+
+# Register your models here.
+# This will take our model visible on admin page
+admin.site.register(Offer)
---
+++ offer/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Offer(models.Model):
+    customerName = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
+    message = models.CharField(max_length=300)
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    
+    def send(self):
+        self.save()
+    
